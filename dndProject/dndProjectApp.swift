827e5ce5355919cf004255f99f2fc72a6f7a2e41
chad <unopoop@gmail.com>
2022-10-26 00:44:31 -0500

    Initial Commit

---
+++ dndProject/dndProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  dndProjectApp.swift
+//  dndProject
+//
+//  Created by Chad Parr on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct dndProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
